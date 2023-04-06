02f0c0af02b3a2495eea79c4f9229e4b27d0962a
Anna Sham <72109977+annasham@users.noreply.github.com>
2023-04-06 19:16:27 -0400

    weather.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ let weather = {
       document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
       document.querySelector(".wind").innerText = "Wind speed: " + speed + "miles/h";
       document.querySelector(".weather").classList.remove("loading");
-      document.body.style.backgroundImage = "url('https://source.unsplash.com/3456x2234/?" + name + "')"
+      document.body.style.backgroundImage = "url('https://source.unsplash.com/2234x3456/?" + name + "')"
     },
     search: function(){
       this.fetchWeather(document.querySelector(".search-bar").value);
